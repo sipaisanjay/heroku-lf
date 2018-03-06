d71b79ba2c21539f9752158ac3e4c106b92f8c7c
Sanjay Sipai <bluerocksta@gmail.com>
2018-03-06 14:45:02 +0545

    First commit

---
+++ src/main/java/com/lfa/web/servlet/DefaultServlet.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lfa.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Sanjay07
+ */
+public class DefaultServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        
+    }
+
+}
